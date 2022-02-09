b486412135a950ce14892609fea0dd0aec6c6773
Dolphin-PC <pcx474@gmail.com>
2022-02-09 21:23:07 +0900

    npm i story book, 에러 복구

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
 import { wrapper } from '../redux/store'
-import GlobalStyle from '../styles/global-style';
-import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,11 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Ani Diary</title>
       </Head>
 
-      {/* <GlobalStyle /> */}
-
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
