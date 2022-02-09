3a5244737daa4c0add9e52863b97325581092a6f
Dolphin-PC <pcx474@gmail.com>
2022-02-09 20:56:54 +0900

    global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 import { wrapper } from '../redux/store'
-import '../styles/globals.css'
+import GlobalStyle from '../styles/global-style';
+import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content='width=device-width, initial-scale=1' />
+        <title>Ani Diary</title>
+      </Head>
+
+      {/* <GlobalStyle /> */}
+
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default wrapper.withRedux(MyApp);
